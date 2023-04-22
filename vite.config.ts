8def48261600458da98af8dd24b45d4a244012ec
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-22 16:14:01 +0300

    +

--- vite.config.ts
+++ vite.config.ts
@@ -6,25 +6,24 @@ import eslint from 'vite-plugin-eslint';
 export default defineConfig({
 	plugins: [react(), eslint()],
 	base: '/aw/',
-		css: {
-			devSourcemap: true
+	css: {
+		devSourcemap: true
+	},
+	resolve: {
+		alias: {
+			'~assets': path.resolve(__dirname, 'src/assets'),
+			'~api': path.resolve(__dirname, 'src/App/api'),
+			'~components': path.resolve(__dirname, 'src/App/components'),
+			'~constants': path.resolve(__dirname, 'src/App/constants'),
+			'~helpers': path.resolve(__dirname, 'src/App/helpers'),
+			'~hooks': path.resolve(__dirname, 'src/App/hooks'),
+			'~pages': path.resolve(__dirname, 'src/App/pages'),
+			'~store': path.resolve(__dirname, 'src/App/store'),
+			'~styles': path.resolve(__dirname, 'src/App/styles'),
+			'~types': path.resolve(__dirname, 'src/App/types'),
+			'~utils': path.resolve(__dirname, 'src/App/utils'),
+			'~app': path.resolve(__dirname, 'src/App'),
+			'~': path.resolve(__dirname, 'src'),
 		},
-		resolve: {
-			alias: {
-				'~assets': path.resolve(__dirname, 'src/assets'),
-				'~api': path.resolve(__dirname, 'src/App/api'),
-				'~components': path.resolve(__dirname, 'src/App/components'),
-				'~constants': path.resolve(__dirname, 'src/App/constants'),
-				'~helpers': path.resolve(__dirname, 'src/App/helpers'),
-				'~hooks': path.resolve(__dirname, 'src/App/hooks'),
-				'~pages': path.resolve(__dirname, 'src/App/pages'),
-				'~store': path.resolve(__dirname, 'src/App/store'),
-				'~styles': path.resolve(__dirname, 'src/App/styles'),
-				'~types': path.resolve(__dirname, 'src/App/types'),
-				'~utils': path.resolve(__dirname, 'src/App/utils'),
-				'~app': path.resolve(__dirname, 'src/App'),
-				'~': path.resolve(__dirname, 'src'),
-			},
-		},
-	}
-)
+	},
+})
